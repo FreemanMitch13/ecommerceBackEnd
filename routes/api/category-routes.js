91e12f6779e09bae1d4678cacb8c08ec07625f6d
Mitchell Freeman <mitchell.w.freeman13@gmail.com>
2022-04-02 10:57:22 -0400

    updated models and routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const res = require('express/lib/response');
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  }).then(data => res.json(data));
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id,{
+    include: [Product]
+  });
+  if (!categoryData) {
+  res.status(404).json({ message: 'No categoryData found with this id!' });
+      return;
+  }
+  res.status(200).json(categoryData);
+} catch (err){
+  res.status(500).json(err);
+}
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      category_id: req.params.category_id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll().then((tagData) => {
+    res.json(tagData);
+  });
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id,{
+    include: [Product]
+  });
+  if (!tagData) {
+  res.status(404).json({ message: 'No tagData found with this id!' });
+      return;
+  }
+  res.status(200).json(tagData);
+} catch (err){
+  res.status(500).json(err);
+}
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:tag_id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      tag_id: req.params.tag_id,
+    },
+  })
+    .then((deletedTag) => {
+      res.json(deletedTag);
+    })
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
